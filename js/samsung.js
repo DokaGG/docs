ca0f7bf3194ead39b48160239e894be8ed61d6a2
DokaGG <57476039+DokaGG@users.noreply.github.com>
2024-10-29 08:29:38 +0530

    update 1.0

---
+++ js/samsung.js
@@ -0,0 +1,9 @@
+// samsung.js
+export const samsungPDFs = [
+    { name: "Samsung PDF 1", file: "./pdf/android/samsung/ANDROID GND RESISTENCE/J5 2016 valores escala de diodos.pdf" },
+    { name: "Samsung PDF 2", file: "./pdf/samsung/document2.pdf" },
+    { name: "Samsung PDF 3", file: "./pdf/samsung/document3.pdf" },
+    { name: "Samsung PDF 4", file: "./pdf/samsung/document4.pdf" },
+    { name: "Samsung PDF 5", file: "./pdf/samsung/document5.pdf" },
+    { name: "Samsung PDF 6", file: "./pdf/samsung/document6.pdf" },
+];
